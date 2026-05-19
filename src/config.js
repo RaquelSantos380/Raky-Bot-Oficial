62297990cfd48adf90fa420fa159815f5634c120
RakyOfficialStore <raquelgata780@gmail.com>
2026-05-18 21:57:06 -0300

    Atualizar o config.js

--- src/config.js
+++ src/config.js
@@ -16,7 +16,7 @@ export const BOT_NAME = "Raky BOT";
 // LID do bot.
 // Para obter o LID do bot, use o comando <prefixo>lid respondendo em cima de uma mensagem do número do bot
 // Troque o <prefixo> pelo prefixo do bot (ex: /lid).
-export const BOT_LID = "12345678901234567890@lid";
+export const BOT_LID = "5562992900737@lid";
 
 // LID do dono do bot.
 // Para obter o LID do dono do bot, use o comando <prefixo>meu-lid
