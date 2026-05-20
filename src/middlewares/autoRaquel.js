9788dceab44ee9a1f565a5bbe0d7a0670ab82d2a
RakyOfficialStore <raquelgata780@gmail.com>
2026-05-19 23:23:54 -0300

    Atualizar o autoRaquel.js

--- src/middlewares/autoRaquel.js
+++ src/middlewares/autoRaquel.js
@@ -5,7 +5,7 @@ export async function autoRaquelHandler(socket, remoteJid, webMessage, fullMessa
 
   if (msg.includes("raquel")) {
     await socket.sendMessage(remoteJid, {
-      text: `🌴 *Raquel está viajando agora!*\n\nInfelizmente ela vai estar ausente por no máximo *3 dias*.\n\nAgradeço pela compreensão! 💜`,
+      text: `🌴 *Raquel está viajando agora!*\n\nInfelizmente ela vai estar ausente por no máximo *2 dias*.\n\nAgradeço pela compreensão! 💜`,
     }, { quoted: webMessage });
     return true;
   }
