f428d8fc561fcf7bc0876340c4704ec70c433fa5
RakyOfficialStore <raquelgata780@gmail.com>
2026-05-25 18:02:33 -0300

    Atualizar o set-prefix.js

--- src/commands/owner/set-prefix.js
+++ src/commands/owner/set-prefix.js
@@ -6,30 +6,20 @@ export default {
   name: "set-prefix",
   description: "Mudo o prefixo de uso dos meus comandos",
   commands: [
-    "set-prefix",
-    "altera-prefix",
-    "altera-prefixo",
-    "alterar-prefix",
-    "alterar-prefixo",
-    "muda-prefix",
-    "muda-prefixo",
-    "mudar-prefix",
-    "mudar-prefixo",
-    "set-prefixo",
+    "set-prefix", "altera-prefix", "altera-prefixo", "alterar-prefix",
+    "alterar-prefixo", "muda-prefix", "muda-prefixo", "mudar-prefix",
+    "mudar-prefixo", "set-prefixo",
   ],
-  usage: `${PREFIX}set-prefix =`,
-  /**
-   * @param {CommandHandleProps} props
-   */
+  usage: `${PREFIX}set-prefix !`,
   handle: async ({ remoteJid, args, sendSuccessReply }) => {
     if (!args.length) {
-      throw new InvalidParameterError("Você deve fornecer um prefixo!");
+      throw new InvalidParameterError("Você deve fornecer um prefixo!\nEx: /set-prefix !");
     }
     if (args.length !== 1) {
       throw new InvalidParameterError("O prefixo deve ser apenas 1 caractere!");
     }
     const newPrefix = args[0];
     setPrefix(remoteJid, newPrefix);
-    await sendSuccessReply(`Prefixo alterado para: ${newPrefix} neste grupo!`);
+    await sendSuccessReply(`✅ Prefixo alterado para: ${newPrefix} neste grupo!`);
   },
 };
